59e3eabfdfd18885ee08d29a48c9fd19e712cced
wxx <405148366@qq.com>
2018-03-14 22:15:18 +0800

    00

---
+++ demo-common/src/main/java/com/example/demo/User.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class User {
+
+	public int id;
+	public int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+}
